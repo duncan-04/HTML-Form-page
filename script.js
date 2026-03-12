8c5222928649eee0474035d25a8541490eecd883
duncan-04 <kgatoduncan04@icloud.com>
2026-03-12 07:45:30 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+<script>
 
+function validateForm(){
+
+let name= document.getElementById("fullname").value;
+let email= document.getElementById("email").value;
+let telephone= document.getElementById("telephone").value;
+
+if(name === "" || email === "" || teelephone === ""){
+alert("Please fill in all required fields");
+return false;
+}
+
+return true;
+
+}
+
+</script>
